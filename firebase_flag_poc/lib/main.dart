f6b990226c3c5a1378a016b43cbc49fd30c7f493
Sebastian <sebastian.vallejos2001@alumnos.ubiobio.cl>
2025-05-20 17:16:21 -0400

    Show message if user is available for beta

--- firebase_flag_poc/lib/main.dart
+++ firebase_flag_poc/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await analytics.setUserProperty(name: 'beta_user', value: 'true');
   runApp(const MyApp());
 }
 
@@ -29,6 +33,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String _message = 'Cargando...';
+  bool _betaAccess = false;
 
   @override
   void initState() {
@@ -41,13 +46,20 @@ class _HomeScreenState extends State<HomeScreen> {
 
     await remoteConfig.setConfigSettings(RemoteConfigSettings(
       fetchTimeout: const Duration(seconds: 10),
-      minimumFetchInterval: const Duration(seconds: 0), // para pruebas
+      minimumFetchInterval: const Duration(seconds: 0),
     ));
 
-    await remoteConfig.fetchAndActivate();
+    await remoteConfig.setDefaults({
+      'user_valid_for_beta': false,
+      'welcome_message': 'Bienvenido a la app',
+    });
 
+    await remoteConfig.fetchAndActivate();
+    print('Beta access before: $_betaAccess');
     setState(() {
       _message = remoteConfig.getString('welcome_message');
+      _betaAccess = remoteConfig.getBool('show_beta_feature');
+      print('Beta access after: $_betaAccess');
     });
   }
 
@@ -55,7 +67,33 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Remote Config Demo')),
-      body: Center(child: Text(_message, style: const TextStyle(fontSize: 24))),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _message,
+              style: const TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            if (_betaAccess)
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  '¡Estás suscrito a la beta!',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
+
